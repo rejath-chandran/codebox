1c82fe4e066593b4cb7bfe6786d746e8b243f29f
rejath chandran <rejathchandran@rejaths-MacBook-Air.local>
2022-02-08 12:54:05 +0530

    first commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/rejath-chandran/snippetbox/pkg/models"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+    // Because Pat matches the "/" path exactly, we can now remove the manual check
+    // of r.URL.Path != "/" from this handler.
+
+    s, err := app.snippets.Latest()
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    app.render(w, r, "home.page.tmpl", &templateData{
+        Snippets: s,
+    })
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+    // Pat doesn't strip the colon from the named capture key, so we need to
+    // get the value of ":id" from the query string instead of "id".
+    id, err := strconv.Atoi(r.URL.Query().Get(":id"))
+    if err != nil || id < 1 {
+        app.notFound(w)
+        return
+    }
+
+    s, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serverError(w, err)
+        }
+        return
+    }
+
+    app.render(w, r, "show.page.tmpl", &templateData{
+        Snippet: s,
+    })
+}
+
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+    app.render(w, r, "create.page.tmpl", nil)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+    // First we call r.ParseForm() which adds any data in POST request bodies
+    // to the r.PostForm map. This also works in the same way for PUT and PATCH
+    // requests. If there are any errors, we use our app.ClientError helper to send
+    // a 400 Bad Request response to the user.
+    err := r.ParseForm()
+    if err != nil {
+        app.clientError(w, http.StatusBadRequest)
+        return
+    }
+
+    // Use the r.PostForm.Get() method to retrieve the relevant data fields
+    // from the r.PostForm map.
+    title := r.PostForm.Get("title")
+    content := r.PostForm.Get("content")
+    expires := r.PostForm.Get("expires")
+
+    // Create a new snippet record in the database using the form data.
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rejath-chandran/snippetbox
+
+go 1.17
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/justinas/alice v1.2.0
+)
+
+require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
